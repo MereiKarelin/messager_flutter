20973ec23fd4dd8070bc62d933c2a5083dd87e21
MereiKarelin <121480227+MereiKarelin@users.noreply.github.com>
2024-01-26 18:58:02 +0600

    start_third_day

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:messager/data/model/response/category_response_model.dart';
 import 'package:messager/features/messages/category_list/category_view.dart';
 import 'package:messager/features/messages/messages_list/messages_view.dart';
 
-//START SECOND DAY
+//START THIRD DAY
 
 void main() {
   runApp(const MyApp());
